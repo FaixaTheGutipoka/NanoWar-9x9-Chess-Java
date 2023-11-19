8a2fc2a16bf6bb5c1b120f221eca34a3f566db33
FaixaTheGutipoka <labibafaiza.karim@torontomu.ca>
2023-11-19 13:03:33 +0600

    update main

--- src/Main_Chess_Game/Main.java
+++ src/Main_Chess_Game/Main.java
@@ -9,13 +9,32 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        //JFrame logo_window = new JFrame();
+        Windows();
 
+    }
+
+    public static void Windows(){
+        ImageIcon icon = new ImageIcon("C:\\Users\\User\\OneDrive\\Pictures\\NanoWar Icon.png");
+
+        JFrame logo_window = new JFrame();
+
+        logo_window.setIconImage(icon.getImage());
+        logo_window.setTitle("NanoWar");
+        logo_window.getContentPane().setBackground(new Color(1, 31, 52));
+        logo_window.setMinimumSize(new Dimension(700, 700));
+        logo_window.setLocationRelativeTo(null);
+
+        ImageIcon image_icon = new ImageIcon("C:\\Users\\User\\OneDrive\\Pictures\\NanoWar Wallpaper.png");
+        JLabel image_label = new JLabel(image_icon);
+        logo_window.getContentPane().add(image_label, BorderLayout.CENTER);
+
+        logo_window.setVisible(true);
+
+        /*
         JFrame main_window = new JFrame();
 
-        // Set the application icon
-        ImageIcon icon = new ImageIcon("C:\\Users\\User\\OneDrive\\Pictures\\NanoWar Icon.png"); // Replace with the path to your icon image
         main_window.setIconImage(icon.getImage());
+        main_window.setTitle("NanoWar");
 
         // Set the background color (behind the chess board)
         main_window.getContentPane().setBackground(new Color(45, 45, 45));
@@ -29,5 +48,6 @@ public class Main {
         main_window.add(chess_board);
 
         main_window.setVisible(true);
+         */
     }
 }
